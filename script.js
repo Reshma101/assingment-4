314d17465da885c0b8cc00494ab9c73e43fd5173
Reshma101 <63300487+Reshma101@users.noreply.github.com>
2020-06-24 12:57:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+  </head>
+<body>
+
+  
+  <script>
+    for(var i=0;i<5;i++){
+
+    var x= ["joe","reshma","harshini","ramya","kishinda"];
+    if(x[i]=="J"||"j"){
+      console.log("Goodbye"+" " + x);
+    }
+    else{
+      console.log("hello"+" "+  x);
+    }
+  }
+  </script>
+</body>
+</html>
